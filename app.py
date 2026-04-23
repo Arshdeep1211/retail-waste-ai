c9c0b2317d796d4ae3134a9d962ad214cdba6a0a
Arshdeep1211 <arshdeep.engg@gmail.com>
2026-04-23 16:47:16 +0200

    Update title and add import statements

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Test App Running")
+from src.forecast import forecast_next_day
+from src.risk import compute_risk
+from src.recommend import recommend_actions
+from src.customer_ai import suggest_campaign
+
+st.title("Imports working")
